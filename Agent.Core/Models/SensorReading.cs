b7722e2084b3d52e06315c1f9992a1239a7694dc
Anirudh9247 <anirudh9247@users.noreply.github.com>
2026-08-02 07:51:25 +0530

    LibreHardwareVisitor & SensorReading<T> model refactoring

--- Agent.Core/Models/SensorReading.cs
+++ Agent.Core/Models/SensorReading.cs
@@ -1,17 +1,20 @@
+using System.Diagnostics.CodeAnalysis;
+
 namespace Agent.Core.Models;
 
-public class SensorReading<T>
+public class SensorReading<T> where T : struct
 {
     public T? Value { get; set; }
     public string Source { get; set; } = "Unavailable";
     public bool IsFallback { get; set; } = false;
     public DateTime TimestampUtc { get; set; } = DateTime.UtcNow;
 
-    public bool HasValue => Value != null;
+    [MemberNotNullWhen(true, nameof(Value))]
+    public bool HasValue => Value.HasValue;
 
     public static SensorReading<T> Empty(string source = "Unavailable") => new()
     {
-        Value = default,
+        Value = null,
         Source = source,
         IsFallback = false,
         TimestampUtc = DateTime.UtcNow
